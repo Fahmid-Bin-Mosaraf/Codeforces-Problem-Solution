ada0de7dd96fd26d3e4c9ff532841b6c7fe70e89
Fahmid10 <fahmidfamel396@gmail.com>
2024-03-19 22:42:46 +0600

    code

---
+++ A_Wrong_Subtraction.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,n;
+    cin>>a>>n;
+    for(int i=0; i<n; i++)
+    {
+        if(a%10==0)
+        {
+            a/=10;
+        }
+        else
+        {
+            a--;
+        }
+    }
+    cout<<a;
+}
