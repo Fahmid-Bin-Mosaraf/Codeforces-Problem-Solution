5064e256f0b3d0e4d5652f324e10447d8d31ef05
Fahmid-Bin-Mosaraf <fahmidfamel396@gmail.com>
2024-11-07 20:14:23 +0600

    new commit

---
+++ A_Ian_Visits_Mary.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long long int lli;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+int main()
+{
+    fast_io;
+    int t;  
+    cin>>t;
+    while(t--)
+    {
+        ll a, b;
+        cin>>a>>b;
+
+        cout<<2<<endl;
+        cout<<a-1<<" "<<1<<endl;
+        cout<<a<<" "<<b<<endl;
+    }
+    return 0;
+}
