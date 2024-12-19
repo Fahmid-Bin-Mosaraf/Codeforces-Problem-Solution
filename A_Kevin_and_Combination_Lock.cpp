fba4a774d52349e4d794f09cf0f5c202e9df8c51
Fahmid-Bin-Mosaraf <fahmidfamel396@gmail.com>
2024-12-19 21:33:16 +0600

    new commit

---
+++ A_Kevin_and_Combination_Lock.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+int main()
+{
+    fast_io;
+    int t;  
+    cin>>t;
+    while(t--)
+    {
+        ll x;
+        cin>>x;
+
+        if(x%33 == 0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
